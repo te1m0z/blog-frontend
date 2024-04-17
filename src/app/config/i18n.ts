09b860f21f416a922edf0a9f32c9425f55be57a4
te1m0z <dmitriy.balamozhnov@gmail.com>
2024-04-17 15:49:48 +0400

    feat: i18n locales done

--- src/app/config/i18n.ts
+++ src/app/config/i18n.ts
@@ -1,19 +1,17 @@
 import i18n from "i18next"
 import { initReactI18next } from "react-i18next"
 import languageDetector from 'i18next-browser-languagedetector'
-
-import en from '@/app/assets/locales/en.json'
-import ru from '@/app/assets/locales/ru.json'
+import HttpApi from 'i18next-http-backend'
 
 i18n
     .use(initReactI18next)
     .use(languageDetector)
+    .use(HttpApi)
     .init({
-        resources: {
-            en,
-            ru
+        backend: {
+            loadPath: '/locales/{{lng}}.json'
         },
-        lng: import.meta.env.VITE_DEFAULT_LOCALE,
+        load: 'currentOnly',
         fallbackLng: import.meta.env.VITE_DEFAULT_LOCALE,
         supportedLngs: ['en', 'ru'],
         detection: {

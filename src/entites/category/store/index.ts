2fba1d943a62fd239d88637c2b4cbfd84b9a8fa0
Te1m0z <dmitriy.balamozhnov@gmail.com>
2024-06-06 21:58:46 +0400

    feat: env production

--- src/entites/category/store/index.ts
+++ src/entites/category/store/index.ts
@@ -1,8 +1,27 @@
 import * as api from './api'
+import { ICategory } from '../types'
+import { makeObservable, observable } from 'mobx'
+
+type TCategoryPrivateFields = '_allCategories'
 
 export class Category {
+
+    private _allCategories: ICategory[] = []
+
+    get allCategories() {
+        return this._allCategories
+    }
+
+    constructor() {
+        makeObservable<Category, TCategoryPrivateFields>(this, {
+            _allCategories: observable,
+        })
+    }
+
     async fetchAll() {
         const { categories } = await api.fetchAll()
+
+        this._allCategories = categories
 
         return categories
     }
